501256932837f8984a97d14f76cfef0a9769e6ef
vikiru <72267229+vikiru@users.noreply.github.com>
2025-05-20 21:48:03 -0400

    chore: configure dev tools for project

---
+++ prettier.config.mjs
@@ -0,0 +1,26 @@
+export default {
+    printWidth: 80,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    trailingComma: 'all',
+    bracketSpacing: true,
+    bracketSameLine: false,
+    arrowParens: 'always',
+    endOfLine: 'auto',
+    singleAttributePerLine: false,
+    plugins: [
+        'prettier-plugin-java',
+        'prettier-plugin-jsdoc',
+        'prettier-plugin-packagejson',
+        'prettier-plugin-prisma',
+        'prettier-plugin-tailwindcss',
+    ],
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            parser: 'typescript',
+        },
+    ],
+};
