f750c3db6385fee4204c6b268cbbbaa171fff7c3
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-02-16 17:28:29 -0500

    feat: add alembic migrations to order service

--- services/order/database.py
+++ services/order/database.py
@@ -1,7 +1,6 @@
 from contextlib import contextmanager
 
-from sqlalchemy import text
-from sqlmodel import Session, SQLModel, create_engine
+from sqlmodel import Session, create_engine
 
 from config import Config
 
@@ -12,18 +11,3 @@ engine = create_engine(Config.DATABASE_URL, echo=True)
 def get_session():
     with Session(engine) as session:
         yield session
-
-
-def init_db():
-    SQLModel.metadata.create_all(engine)
-
-    with Session(engine) as session:
-        session.exec(text('DELETE FROM orderitem'))
-        session.exec(text('DELETE FROM "order"'))
-        session.execute(text('ALTER SEQUENCE order_id_seq RESTART WITH 1'))
-        session.execute(text('ALTER SEQUENCE orderitem_id_seq RESTART WITH 1'))
-        session.commit()
-
-
-if __name__ == '__main__':
-    init_db()
