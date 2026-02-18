8e54b0a9d74d0e95fb21e417e91bac5a1e3275e3
Visakan Kirubakaran <72267229+vikiru@users.noreply.github.com>
2026-02-18 16:22:47 -0500

    chore: commit recent local changes

--- services/product/alembic/env.py
+++ services/product/alembic/env.py
@@ -3,8 +3,7 @@ import sys
 from logging.config import fileConfig
 
 from dotenv import load_dotenv
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 from sqlmodel import SQLModel
 
 from alembic import context
